1baa7db728299bf07293d1943dcd206a35a6040b
TangoCash <eric@loxat.de>
2016-11-29 18:24:59 +0100

    fix after merge

--- src/gui/movieplayer.h
+++ src/gui/movieplayer.h
@@ -270,9 +270,6 @@ class CMoviePlayerGui : public CMenuTarget
 
 	static CMoviePlayerGui& getInstance(bool background = false);
 
-	MI_MOVIE_INFO * p_movie_info;
-	std::string	file_name;
-	std::string	pretty_name;
 	int exec(CMenuTarget* parent, const std::string & actionKey);
 	bool Playing() { return playing; };
 	bool osdTimeVisible() { return FileTime.IsVisible(); };
@@ -280,7 +277,6 @@ class CMoviePlayerGui : public CMenuTarget
 	int GetSpeed() { return speed; }
 	int GetPosition() { return position; }
 	int GetDuration() { return duration; }
-	int getState() { return playstate; }
 	void UpdatePosition();
 	int timeshift;
 	int file_prozent;
